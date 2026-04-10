23a0c7226fdf7fb2ab23a0b85851cf2b98cdb2df
akashbrs <methukuakash092@gmail.com>
2026-04-10 21:59:17 +0530

    added database of postgresql

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +82,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://coresentinal_database_user:Tlvq44VX04yCAfcX6PJtusjkjjXkOmyN@dpg-d7ch631j2pic73bt8m20-a.oregon-postgres.render.com/coresentinal_database',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
